c0e108ee17e4418c4d34e8d21ab026a776ea7568
ChSt19 <chrissteffen98@gmail.com>
2023-11-28 10:53:49 +0100

    Added testing thread

---
+++ STM32F4_RODOS_Template/Debug/Testing_Thread.cpp
@@ -0,0 +1,52 @@
+#include "Testing_Thread.hpp"
+
+#include "rodos.h"
+#include "matlib.h"
+
+#include "../control/AttitudeEstimation.hpp"
+#include "../hardware/imu.hpp"
+#include "../hardware/ReactionwheelEncoder.hpp"
+#include "../hardware/hbridge.hpp"
+
+#include "simple_pid.hpp"
+
+TestingThread::TestingThread()
+{
+
+}
+
+void TestingThread::init()
+{
+
+	hbridge.initialization();
+	encoder.Init();
+
+}
+
+void TestingThread::run()
+{
+
+	VCTR::CTRL::SimplePID pid(0.1, 0, 0, 0.4);
+
+	while (true)
+	{
+
+		float wheelVel = encoder.getSpeed().data;
+
+		float wheelPwr = pid.update(wheelVel);
+		wheelPwr = sinf(SECONDS_NOW() * 0.1) * 0.5f;
+
+		hbridge.setVoltage(wheelPwr);
+
+		PRINTF("Power: %.2f [%], Encoder: %.2f [Rad/s]", wheelPwr, wheelVel);
+
+		suspendCallerUntil(NOW() + period * MILLISECONDS);
+		
+	}
+
+}
+
+
+//DebugThread debugthread;
+//Semaphore mode_protec;
+//modes mode = Idle;
---
+++ STM32F4_RODOS_Template/Debug/Testing_Thread.hpp
@@ -0,0 +1,33 @@
+#ifndef FLOATSAT_THREADS_DEBUGTHREAD_HPP_
+#define FLOATSAT_THREADS_DEBUGTHREAD_HPP_
+
+#include "rodos.h"
+#include "matlib.h"
+
+#include "../control/AttitudeEstimation.hpp"
+#include "../hardware/imu.hpp"
+#include "../timestamp.hpp"
+
+
+/**
+ * @brief Use this class/thread for testing different functions.
+*/
+class TestingThread : public Thread
+{
+public:
+
+	// Set name, prio and stack size
+	TestingThread();// : Thread("Debug Thread", 100, 2000) {} 
+
+	void init();
+	void run();
+
+private:
+
+	const int period = 10;		// [ms]
+};
+
+
+extern TestingThread debugthread;
+
+#endif 
